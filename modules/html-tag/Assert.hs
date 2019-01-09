19712df4b74dd25554e78279c3550cf658bdc4d3
brymlee <brymlee1911@gmail.com>
2019-01-09 13:56:21 -0800

    Getting rid of copied dependencies

--- modules/html-tag/Assert.hs
+++
@@ -1,4 +0,0 @@
-module Assert where 
-assertEquals :: (Show a, Eq a) => String -> a -> a -> IO()
-assertEquals testLabel a b | a /= b = error $ "Test " ++ testLabel ++ " failed: Actual value " ++ (show a) ++ " does not equal expected " ++ (show b)
-                           | a == b = putStr ""
